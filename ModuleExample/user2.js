b87933a47f4d5a35dbb7c4c8937f16895a8c18e2
Seongseokwon <pam9411@naver.com>
2020-09-14 23:37:37 +0900

    module study

---
+++ ModuleExample/user2.js
@@ -0,0 +1,12 @@
+exports = {
+    getUser: function () {
+        return {
+            id: 'test01',
+            name: 'hello'
+        };
+    },
+    group: {
+        id: 'group01',
+        name: 'friend'
+    }
+}
